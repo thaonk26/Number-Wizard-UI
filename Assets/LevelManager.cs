562c362243e46cc4e941fa0379ad67f092640b6a
Nathan <thaonk26@uww.edu>
2017-01-14 06:24:51 -0600

    initial upload

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name)
+    {
+        Debug.Log("Level load requested for: " + name);
+        Application.LoadLevel(name);
+    }
+    public void QuitRequest()
+    {
+        Debug.Log("Level quit");
+        Application.Quit();
+    }
+}
---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour
+{
+
+    int max;
+    int min;
+    int guess;
+    public int maxGuessesAlowed = 6;
+    public Text text;
+
+    void Start()
+    {
+        StartGame();
+
+    }
+
+
+    // Use this for initialization
+    void StartGame()
+    {
+        max = 1000;
+        min = 1;
+        NextGuess();
+
+    }
+
+    public void GuessLower()
+    {
+        max = guess;
+        NextGuess();
+    }
+    public void GuessHigher()
+    {
+        min = guess;
+        NextGuess();
+    }
+    void NextGuess()
+    {
+        guess = Random.Range(min, max + 1);
+        text.text = guess.ToString();
+        maxGuessesAlowed -= 1;
+        if(maxGuessesAlowed <= 0)
+        {
+            Application.LoadLevel("Win");
+        }
+    }
+
+}
